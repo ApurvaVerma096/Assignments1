27fecc973b7d31309fb9f0cb9f8b248496a01b1b
ApurvaVerma096 <36083184+ApurvaVerma096@users.noreply.github.com>
2019-05-25 15:49:35 +0530

    Delete ServiceInterface.java

--- BankApplication/src/com/ibm/test/service/ServiceInterface.java
+++
@@ -1,9 +0,0 @@
-package com.ibm.test.service;
-
-public interface ServiceInterface {
-	
-	void storeIntoAccountDetail();
-	void stroreIntoUserDetails(String fname,String lname,long mobno,String ib,String passwrd);
-	boolean validatePassword(int uid, String paswrd);
-	
-}
