526afa4649fb5a4c2952d7d3af2bab9f9b716069
ApurvaVerma096 <36083184+ApurvaVerma096@users.noreply.github.com>
2019-05-25 15:49:15 +0530

    Delete DaoInterface.java

--- BankApplication/src/com/ibm/test/dao/DaoInterface.java
+++
@@ -1,7 +0,0 @@
-package com.ibm.test.dao;
-
-public interface DaoInterface {
-
-	void storeUserDetails(String fname,String lname,long mobno,String ib,String passwrd);
-	boolean validatePassword(int uid,String paswrd);
-}
